249a2e4f8d86403a6807bd41c036a9bc8410f445
Ryan Santos <ryansantos86@gmail.com>
2020-11-23 12:42:25 -0800

    bugfix: not existing invaders still registered points

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,9 +1,12 @@
+import 'regenerator-runtime/runtime';
+
 import { Rect } from './types';
 import { LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, SPACE_BAR } from './constants';
 
 const earth = document.getElementById('earth') as HTMLElement;
 const columns = document.getElementsByClassName('invaders-col') as HTMLCollection; // prettier-ignore
 const gun = document.getElementById('gun') as HTMLElement;
+const score = document.getElementById('score-count') as HTMLElement;
 const bulletWidth = 6;
 
 let invaders: Element[];
@@ -32,9 +35,16 @@ class Player {
     moveRight () {
         this.stopMoving();
         this.player.style.left = `${(this.xDir += 5)}px`;
-
         if (this.player.offsetLeft >= 1010) this.stopMoving();
         else this.moveID = requestAnimationFrame(this.moveRight);
+    }
+
+    async scorePoints () {
+        for (let i = 1; i <= 10; i++) {
+            this.score++;
+            score.textContent = this.score.toString();
+            await sleep(25);
+        }
     }
 
     stopMoving () {
@@ -55,45 +65,35 @@ const player: Player = new Player();
 
 const update = () => {
     if (gun.children.length) {
-        Array.from(gun.children).forEach((bullet: any) => {
+        [...gun.children].forEach((bullet: any) => {
             bullet.style.top = `${bullet.offsetTop - 5}px`;
-            detectCollisions(bullet);
+            updateCollisions(bullet);
         });
     }
     requestAnimationFrame(update);
 };
 
-const detectCollisions = (bullet: HTMLElement) => {
+const updateCollisions = async (bullet: HTMLElement) => {
     for (const invader of invaders) {
-        if (collision(rect(bullet), rect(invader))) {
+        if (checkCollision(rectOf(bullet), rectOf(invader))) {
             bullet.remove();
             invader.remove();
-        } else if (bullet.offsetTop <= -earth.offsetHeight) {
+            player.scorePoints();
+        }
+        if (bullet.offsetTop <= -earth.offsetHeight) {
             bullet.remove();
         }
     }
 };
 
-const collision = (r1: Rect, r2: Rect) => {
+const checkCollision = (r1: Rect, r2: Rect) => {
     return (
+        r2.width &&
         r1.x + r1.width >= r2.x &&
         r1.x <= r2.x + r2.width &&
         r1.y + r1.height >= r2.y &&
         r1.y <= r2.y + r2.height
     );
-};
-
-const rect = (el: Element) => el.getBoundingClientRect();
-
-const renderInvaders = () => {
-    Array.from(columns).forEach((col: Element) => {
-        col.innerHTML = '<div class="invader"></div>'.repeat(5);
-    });
-    invaders = Array.from(document.getElementsByClassName('invader'));
-};
-
-const renderPlayer = () => {
-    (document.getElementById('player-zone') as HTMLElement).appendChild(player.html()) // prettier-ignore
 };
 
 const onKeydown = (e: KeyboardEvent) => {
@@ -103,13 +103,32 @@ const onKeydown = (e: KeyboardEvent) => {
     if (e.keyCode === SPACE_BAR) player.fire();
 };
 
+const rectOf = (el: Element) => {
+    return el.getBoundingClientRect();
+};
+
+const sleep = (ms = 0) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const renderInvaders = () => {
+    [...columns].forEach((col: Element) => {
+        col.innerHTML = '<div class="invader"></div>'.repeat(5);
+    });
+    invaders = Array.from(document.getElementsByClassName('invader'));
+};
+
+const renderPlayer = () => {
+    (document.getElementById('player-zone') as HTMLElement).appendChild(player.html()) // prettier-ignore
+};
+
 (() => {
-    const docReady = () => {
+    const drawGame = () => {
         renderInvaders();
         renderPlayer();
         update();
     };
 
     window.addEventListener('keydown', onKeydown);
-    window.addEventListener('load', docReady);
+    window.addEventListener('load', drawGame);
 })();
